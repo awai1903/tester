dec250f05a41ed11d865627f11c0f63f32ce0f23
awai1903 <cmkiller786@gmail.com>
2025-07-22 01:13:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function showWish(img, message) {
-  // clear all previous wishes
-  document.querySelectorAll('.wish-box').forEach(box => box.innerHTML = '');
-  
-  // show wish below clicked image
-  const wishBox = img.nextElementSibling;
-  wishBox.innerHTML = `<p>${message}</p>`;
-}
+<script>
+  const wishes = [
+    "🎉 Happy Birthday, shining star! 💜",
+    "Wishing you a magical day filled with joy and love.",
+    "May your dreams sparkle like your smile!",
+    "You are more beautiful than any BTS melody 🎶",
+    "Taehyung is proud of you today 💜",
+    "Sending purple vibes for your happiest birthday!",
+    "You light up the world just like BTS lights up the stage!",
+  ];
+
+  function showWish(memberEl, index) {
+    const allWishes = document.querySelectorAll('.wish');
+    allWishes.forEach(w => w.style.display = 'none'); // Hide all previous
+
+    const wishBox = memberEl.querySelector('.wish');
+    wishBox.innerText = wishes[index % wishes.length];
+    wishBox.style.display = 'block';
+  }
+</script>
